7e73472075da1aff1950d2796cad735b7ef51264
jera <iamjera@proton.me>
2023-06-26 23:33:24 +0500

    Restructured project

--- easy/1. Two Sum.go
+++ easy/1. Two Sum.go
@@ -9,9 +9,9 @@ func main() {
 }
 
 func twoSum(nums []int, target int) []int {
-	for i, val := range nums {
-		for j, value := range nums {
-			if i != j && val+value == target {
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums); j++ {
+			if i != j && nums[i]+nums[j] == target {
 				return []int{i, j}
 			}
 		}
